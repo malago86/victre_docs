2a081a08619f87d60520750008a1c7bc32c3e47f
Miguel Lago <miguelangel.lago@gmail.com>
2020-12-11 15:20:24 -0500

    Added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## VICTRE PIPELINE DOCUMENTATION
+
+**Author**: Miguel A. Lago
+
+## DOCUMENTATION
+
+You can find the documentation in this link: https://malago86.github.io/victre_docs/annotated.html
+
+Report your issues and requests here: https://github.com/malago86/VICTRE_PIPELINE
--- search/files_72.js
+++ search/files_72.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
+  ['readme_2emd',['README.md',['../Victre_2breastMass_2README_8md.html',1,'']]],
   ['run_5fpipeline_2epy',['run_pipeline.py',['../run__pipeline_8py.html',1,'']]],
   ['run_5fpipeline_5flesionsizes_2epy',['run_pipeline_lesionsizes.py',['../run__pipeline__lesionsizes_8py.html',1,'']]]
 ];
